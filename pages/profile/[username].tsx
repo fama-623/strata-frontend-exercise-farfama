bd563ea8fbdef7c1f62bb2e369cf770a91d8fda8
Farfama <farfama.hargaaya@gmail.com>
2023-02-20 19:53:07 +0900

    Update profile styling

--- pages/profile/[username].tsx
+++ pages/profile/[username].tsx
@@ -8,9 +8,8 @@ const User = (profileData: ProfileData) => {
 
 
   return (
-
-      <div className="w-full h-80 flex flex-col items-center justify-center space-y-12">
-      <h1>{profileData.username}'s Profile</h1>
+      <div className="flex  items-center justify-center h-screen cursor-pointer">
+      <div className="max-w-lg w-96 p-8 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
       <Image
         src={`/users/${profileData.username}.png`}
         alt={`/users/${profileData.username}'s profile image`}
@@ -18,11 +17,13 @@ const User = (profileData: ProfileData) => {
         width={50}
         height={50}
       />
-        <p>Bio: {profileData.bio}</p>
+        <h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{profileData.username}</h1>
+        {/* <p className="font-normal text-gray-700 dark:text-gray-400">Bio: {profileData.bio}</p> */}
         <p>Age: {profileData.age}</p>
         <p>Email: {profileData.email}</p>
         <p>Twitter: {profileData.twitter}</p>
-        <p>Birthday: {profileData.birthday}</p>
+        {/* <p>Birthday: {profileData.birthday}</p> */}
+      </div>
     </div>
     )
 }
