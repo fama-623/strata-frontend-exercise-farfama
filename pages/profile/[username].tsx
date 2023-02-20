258f0992f9c879822a95a2dcb744448a746310d0
Farfama <farfama.hargaaya@gmail.com>
2023-02-20 21:52:19 +0900

    Add heart svg, styling

--- pages/profile/[username].tsx
+++ pages/profile/[username].tsx
@@ -2,6 +2,7 @@ import { GetServerSideProps } from "next"
 import Image from "next/image"
 import { FC } from "react"
 import {useState, useEffect} from 'react'
+import Heart from "../../components/heart"
 
 
 
@@ -24,24 +25,27 @@ const User = ({username, age, email, twitter}: ProfileData) => {
   };
 
   return (
-      <div className="flex  items-center justify-center h-screen cursor-pointer">
-      <div className="max-w-lg w-96 p-8 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      <div className="flex items-center justify-center h-screen">
+      <div className="max-w-lg w-96 p-8 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
       <Image
         src={`/users/${username}.png`}
         alt={`/users/${username}'s profile image`}
-        className="rounded-full"
-        width={50}
-        height={50}
+        className="rounded-full mb-6"
+        width={100}
+        height={100}
       />
-        <h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{username}</h1>
+        <h1 className="mb-2 text-2xl font-bold tracking-tight text-slate-600	 dark:text-white">{username}</h1>
         {/* <p className="font-normal text-gray-700 dark:text-gray-400">Bio: {profileData.bio}</p> */}
-        <p>Age: {age}</p>
-        <p>Email: {email}</p>
-        <p>Twitter: {twitter}</p>
+        <p className="text-slate-600	">Age: {age}</p>
+        <p className="text-slate-600	">Email: {email}</p>
+        <p className="text-slate-600	">Twitter: {twitter}</p>
         {/* <p>Birthday: {profileData.birthday}</p> */}
-        <button onClick={handleLikeClick}>
-        {liked ? 'Unlike' : 'Like'}
-      </button>
+        <div className="flex mt-6 items-center justify-between">
+          <button className="text-slate-500	" onClick={handleLikeClick}>
+          {liked ? 'Unlike' : 'Like'}
+          </button>
+          {liked && <Heart/>}
+        </div>
       </div>
     </div>
     )
