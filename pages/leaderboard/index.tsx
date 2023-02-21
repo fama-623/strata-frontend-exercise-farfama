13efa4fbfc5d6d123ca9371cbc70bd0c94b8e77c
Farfama <farfama.hargaaya@gmail.com>
2023-02-21 12:13:45 +0900

    Update prettier formatting

--- pages/leaderboard/index.tsx
+++ pages/leaderboard/index.tsx
@@ -1,49 +1,57 @@
-import { FC } from "react"
-import { useEffect, useState } from "react"
-import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next"
-import Heart from "../../components/Heart"
-import Link from "next/link"
-import Image from "next/image"
+import { FC } from "react";
+import { useEffect, useState } from "react";
+import { GetServerSideProps } from "next";
+import Heart from "../../components/Heart";
+import Link from "next/link";
+import Image from "next/image";
 
-
-
-const Leaderboard: FC<LeaderboardData> = ({leaderboard}) => {
-
+const Leaderboard: FC<LeaderboardData> = ({ leaderboard }) => {
   const [liked, setLiked] = useState<any>({});
   useEffect(() => {
     const items = { ...localStorage };
-    setLiked(items)
+    setLiked(items);
   }, []);
 
   console.log(liked);
 
-
-
-  return (<>
-    <div className="">
-      <div className="flex  items-center justify-center flex-col">
-      <h1 className="text-4xl font-bold m-12">Leaderboard</h1>
-      {leaderboard.map((user : UserDetails, index: number) => (
-        <Link className="" href={`/profile/${user.username}`} key={user.username}>
-        <div className="max-w-lg w-96 p-8 mb-4 mt-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        <h2 className="text-center mb-8 text-2xl font-bold tracking-tight text-slate-600">{user.username}</h2>
-          <Image
-            src={user.profileImage}
-            alt={`${user.username} profile image`}
-            className="rounded-full m-auto"
-            width={100}
-            height={100}
-            
-          />
-        <div className="flex mt-6 items-center justify-between">
-          <p className="">{`Points: ${user.score}`}</p>
-          <p>{ (liked.hasOwnProperty(`liked_${user.username}`) && liked[`liked_${user.username}`]=="true") ? <Heart/> : ""}</p>
+  return (
+    <>
+      <div>
+        <div className="flex  items-center justify-center flex-col">
+          <h1 className="text-4xl font-bold m-12">Leaderboard</h1>
+          {leaderboard.map((user: UserDetails, index: number) => (
+            <Link
+              href={`/profile/${user.username}`}
+              key={user.username}
+            >
+              <div className="max-w-lg w-96 p-8 mb-4 mt-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <h2 className="text-center mb-8 text-2xl font-bold tracking-tight text-slate-600">
+                  {user.username}
+                </h2>
+                <Image
+                  src={user.profileImage}
+                  alt={`${user.username} profile image`}
+                  className="rounded-full m-auto"
+                  width={100}
+                  height={100}
+                />
+                <div className="flex mt-6 items-center justify-between">
+                  <p className="">{`Points: ${user.score}`}</p>
+                  <p>
+                    {liked.hasOwnProperty(`liked_${user.username}`) &&
+                    liked[`liked_${user.username}`] == "true" ? (
+                      <Heart />
+                    ) : (
+                      ""
+                    )}
+                  </p>
+                </div>
+              </div>
+            </Link>
+          ))}
         </div>
-        </div>
-        </Link>
-      ))}</div>
-    </div>
-  </>
+      </div>
+    </>
   );
 };
 
@@ -53,9 +61,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
   const leaderboard = data.leaderboard;
 
   return {
-    props: { leaderboard }
+    props: { leaderboard },
   };
-}
+};
 
-
-export default Leaderboard
+export default Leaderboard;
--- pages/profile/[username].tsx
+++ pages/profile/[username].tsx
@@ -1,15 +1,12 @@
-import { GetServerSideProps } from "next"
-import Image from "next/image"
-import Link from "next/link"
-import { FC } from "react"
-import {useState, useEffect} from 'react'
-import Heart from "../../components/Heart"
-import BackButton from "../../components/BackButton"
+import { GetServerSideProps } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import { FC } from "react";
+import { useState, useEffect } from "react";
+import Heart from "../../components/Heart";
+import BackButton from "../../components/BackButton";
 
-
-
-const User = ({username, age, email, twitter}: ProfileData) => {
-
+const User = ({ username, age, email, twitter }: ProfileData) => {
   const [liked, setLiked] = useState(false);
   // Read the liked status from local storage when the component mounts
   useEffect(() => {
@@ -27,16 +24,18 @@ const User = ({username, age, email, twitter}: ProfileData) => {
   };
 
   return (
-      <div className="flex items-center justify-center h-screen flex-col">
+    <div className="flex items-center justify-center h-screen flex-col">
       <div className="max-w-lg w-96 p-8 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-      <Image
-        src={`/users/${username}.png`}
-        alt={`/users/${username}'s profile image`}
-        className="rounded-full mb-6"
-        width={100}
-        height={100}
-      />
-        <h1 className="mb-2 text-2xl font-bold tracking-tight text-slate-600	 dark:text-white">{username}</h1>
+        <Image
+          src={`/users/${username}.png`}
+          alt={`/users/${username}'s profile image`}
+          className="rounded-full mb-6"
+          width={100}
+          height={100}
+        />
+        <h1 className="mb-2 text-2xl font-bold tracking-tight text-slate-600	 dark:text-white">
+          {username}
+        </h1>
         {/* <p className="font-normal text-gray-700 dark:text-gray-400">Bio: {profileData.bio}</p> */}
         <p className="text-slate-600	">Age: {age}</p>
         <p className="text-slate-600	">Email: {email}</p>
@@ -44,35 +43,36 @@ const User = ({username, age, email, twitter}: ProfileData) => {
         {/* <p>Birthday: {profileData.birthday}</p> */}
         <div className="flex mt-6 items-center justify-between">
           <button className="text-slate-500	" onClick={handleLikeClick}>
-          {liked ? 'Unlike' : 'Like'}
+            {liked ? "Unlike" : "Like"}
           </button>
-          {liked && <Heart/>}
+          {liked && <Heart />}
         </div>
       </div>
       <Link href="/leaderboard" className="mt-8 max-w-lg w-96">
-        <BackButton/>
+        <BackButton />
       </Link>
-      </div>
-    )
-}
+    </div>
+  );
+};
 
+export const getServerSideProps: GetServerSideProps = async ({
+  params,
+}: any) => {
+  const { username } = params;
+  const info = await fetch(`${process.env.URL}/api/profile/${username}`).then(
+    (response) => response.json()
+  );
 
+  return {
+    props: {
+      username: username,
+      bio: info.bio,
+      age: info.age,
+      twitter: info.twitter,
+      email: info.email,
+      birthday: info.birthday,
+    },
+  };
+};
 
-export const getServerSideProps: GetServerSideProps = async ({params}: any) => {
-  const {username} = params;
-  const info = await fetch(`${process.env.URL}/api/profile/${username}`)
-    .then(response => response.json() )
-
-  return{
-      props: {
-            username: username,
-            bio: info.bio,
-            age: info.age,
-            twitter: info.twitter,
-            email: info.email,
-            birthday: info.birthday
-      }
-  }
-}
-
-export default User
+export default User;
